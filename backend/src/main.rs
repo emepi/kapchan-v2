3ae814fecbfa88ae9643030d108e5a9d57cbba25
emepi <elmeri.kemppainen@gmail.com>
2023-11-05 19:02:02 +0200

    Add env logger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,35 +1,46 @@
 use std::env;
 
 use actix_web::{HttpServer, App, web};
-use diesel_async::pooled_connection::{
-    deadpool::Pool, 
-    AsyncDieselConnectionManager,
+use diesel_async::{
+    AsyncMysqlConnection,
+    pooled_connection::{AsyncDieselConnectionManager, deadpool::Pool}, 
 };
 use dotenvy::dotenv;
+use log::info;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Load environment variables.
     dotenv().ok();
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+        .app_data(web::Data::new(mysql_connection_pool()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+fn mysql_connection_pool() -> Pool<AsyncMysqlConnection> {
 
     let mysql_url = env::var("DATABASE_URL").expect(r#"
-        Env variable `DATABASE_URL` must be set in `backend/.env` file.
-        See the .env.example.
+        env variable `DATABASE_URL` must be set in `backend/.env`
+        see: .env.example
     "#);
+
+    info!("connecting to mysql server");
 
     let mysql_connection_pool = Pool::builder(
         AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>
         ::new(mysql_url)
     )
     .build()
-    .expect("Mysql connection pool was unable to start");
+    .expect("failed to establish connection pooling");
 
-    HttpServer::new(move || {
-        App::new()
-        .app_data(web::Data::new(mysql_connection_pool.clone()))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    info!("connection pool initialized");
+
+    mysql_connection_pool
 }
