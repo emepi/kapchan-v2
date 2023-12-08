a2ee05757c24d96ac52aa6c807dc1d77cbb52017
emepi <elmeri.kemppainen@gmail.com>
2023-12-08 14:08:45 +0200

    Flag models & insert

--- backend/src/board_service/board.rs
+++ backend/src/board_service/board.rs
@@ -63,6 +63,23 @@ impl BoardModel<'_> {
             Err(_) => None,
         }
     }
+
+    pub async fn insert_with_flags(
+        &self,
+        conn_pool: &Pool<AsyncMysqlConnection>,
+        flags: Vec<BoardFlagModel>,
+    ) {
+        todo!()
+    }
+}
+
+#[derive(Queryable, Identifiable, Selectable, Serialize)]
+#[diesel(table_name = board_flags)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+pub struct BoardFlag {
+    id: u32,
+    board_id: u32,
+    flag: u8,
 }
 
 #[derive(Insertable)]
@@ -70,6 +87,37 @@ impl BoardModel<'_> {
 pub struct BoardFlagModel {
     pub board_id: u32,
     pub flag: u8,
+}
+
+impl BoardFlagModel {
+    pub async fn insert(
+        &self,
+        conn_pool: &Pool<AsyncMysqlConnection>,
+    ) -> Option<BoardFlag> {
+
+        match conn_pool.get().await {
+            Ok(mut conn) => {
+                conn.transaction::<_, Error, _>(|conn| async move {
+        
+                    let _ = diesel::insert_into(board_flags::table)
+                    .values(self)
+                    .execute(conn)
+                    .await?;
+        
+                    let flag = board_flags::table
+                    .find(last_insert_id())
+                    .first::<BoardFlag>(conn)
+                    .await?;
+                    
+                    Ok(flag)
+                }.scope_boxed())
+                .await
+                .ok()
+            },
+
+            Err(_) => None,
+        }
+    }
 }
 
 sql_function!(fn last_insert_id() -> Unsigned<Integer>);
