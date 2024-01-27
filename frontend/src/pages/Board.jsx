fd20603a477d40ba11c1d7086b3e9d11ea5bb214
emepi <elmeri.kemppainen@gmail.com>
2024-01-27 13:25:13 +0200

    Thread editor

--- frontend/src/pages/Board.jsx
+++ frontend/src/pages/Board.jsx
@@ -1,7 +1,8 @@
 import { Show, createSignal } from "solid-js"
 import { RadioButton } from "../components/RadioButton"
 import { AccessLevel } from "../scripts/user"
-import { Portal } from "solid-js/web"
+import { boards } from "../scripts/boards"
+import { session } from ".."
 
 
 export const Board = () => {
@@ -9,12 +10,10 @@ export const Board = () => {
     <>
       <nav class="board-selector">
         <div class="navbar">
-          <RadioButton name="board" accessLevel={AccessLevel.Anonymous} checked>etusivu</RadioButton>
-          <RadioButton name="board" accessLevel={AccessLevel.Anonymous}>satunnainen</RadioButton>
-          <RadioButton name="board" accessLevel={AccessLevel.Anonymous}>anime</RadioButton>
-          <RadioButton name="board" accessLevel={AccessLevel.Anonymous}>pelit</RadioButton>
-          <RadioButton name="board" accessLevel={AccessLevel.Anonymous}>teknologia</RadioButton>
-          <RadioButton name="board" accessLevel={AccessLevel.Member}>hikky</RadioButton>
+          <RadioButton name="board" accessLevel={AccessLevel.Anonymous} value="" checked>etusivu</RadioButton>
+          <For each={boards}>
+            { (board) => <RadioButton name="board" accessLevel={board.accessLevel} value={board.handle}>{board.name}</RadioButton> }
+          </For>
         </div>
         <PostingModalButton />
       </nav>
@@ -32,11 +31,47 @@ const PostingModalButton = (props) => {
         luo lanka
       </button>
       <Show when={open()}>
-        <Portal>
+        <div class="post-modal-bg">
           <div class="post-modal">
-            posting..
+            <header class="modal-head">
+              <h3>Luo lanka</h3>
+              <button class="close-btn" onClick={() => setOpen(false)}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path fill="currentColor" d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+              </button>
+            </header>
+            <form class="post-form">
+              <label>
+                <input class="post-subject" type="text" name="subject" placeholder="Otsikko" maxLength="100" />
+              </label>
+              <label class="dropdown">
+                <select class="post-board" name="board">
+                  <option value="" disabled selected>Valitse lauta</option>
+                  <For each={boards}>
+                    { (board) => 
+                    <Show 
+                      when={session() && session().role >= board.accessLevel}
+                      fallback={<option disabled>{board.name}</option>}
+                    >
+                      <option value={board.handle}>{board.name}</option>
+                    </Show>
+                    }
+                  </For>
+                </select>
+                <svg class="drop-icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-360 280-560h400L480-360Z"/></svg>
+              </label>
+              <textarea class="post-body" name="body" placeholder="Sisältö" rows="15" cols="80" maxLength="40000"/>
+              <div class="post-selector board-selector">
+                <label>
+                  <input type="file" name="attachment" accept="image/png, image/jpeg" hidden/>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path fill="currentColor" d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z"/></svg>
+                </label>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path fill="currentColor" d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
+                </button>
+              </div>
+            </form>
           </div>
-        </Portal>
+        </div>
       </Show>
     </div>
   )
---
+++ frontend/src/scripts/boards.js
@@ -0,0 +1,29 @@
+import { AccessLevel } from "./user";
+
+export const boards = [
+  {
+    name: "satunnainen",
+    handle: "b",
+    accessLevel: AccessLevel.Anonymous,
+  },
+  {
+    name: "anime",
+    handle: "a",
+    accessLevel: AccessLevel.Anonymous,
+  },
+  {
+    name: "pelit",
+    handle: "g",
+    accessLevel: AccessLevel.Anonymous,
+  },
+  {
+    name: "teknologia",
+    handle: "g",
+    accessLevel: AccessLevel.Anonymous,
+  },
+  {
+    name: "hikky",
+    handle: "h",
+    accessLevel: AccessLevel.Member,
+  },
+]
