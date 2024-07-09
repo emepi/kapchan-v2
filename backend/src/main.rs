0d3d086ff87e062a8d3c1599f3d1817dde677cac
emepi <elmeri.kemppainen@gmail.com>
2024-07-09 15:44:10 +0300

    Add boilerplate

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_web::{web, App, HttpServer};
+use diesel_async::{
+    AsyncMysqlConnection,
+    pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager}
+};
+use dotenvy::dotenv;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Load environment variables.
+    dotenv().ok();
+    env_logger::init();
+
+    // Init mysql connection pool.
+    let mysql_url = env::var("DATABASE_URL").expect(r#"
+        env variable `DATABASE_URL` must be set in `backend/.env`
+        see: .env.example
+    "#);
+
+    let mysql_connection_pool = Pool::builder(
+        AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>
+        ::new(mysql_url)
+    )
+    .build()
+    .expect("failed to establish connection pooling");
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(web::Data::new(mysql_connection_pool.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- backend/target/debug/deps/backend-4fda7055582520ea.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Elmeri\Projects\kapchan-v2\backend\target\debug\deps\libbackend-4fda7055582520ea.rmeta: src\main.rs
-
-C:\Users\Elmeri\Projects\kapchan-v2\backend\target\debug\deps\backend-4fda7055582520ea.d: src\main.rs
-
-src\main.rs:
