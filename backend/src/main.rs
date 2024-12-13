6fb84d7539b766ebdf787ca75d4b61e26159c820
emepi <elmeri.kemppainen@gmail.com>
2024-12-13 17:14:44 +0200

    Database connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,31 @@
-use actix_web::{App, HttpServer};
+use std::env;
+
+use actix_web::{web, App, HttpServer};
+use diesel_async::pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager};
+use dotenvy::dotenv;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    // Load environment variables.
+    dotenv().ok();
+    
+    // Initialize database connection pool.
+    let mysql_url = env::var("DATABASE_URL").expect(r#"
+        env variable `DATABASE_URL` must be set in `backend/.env`
+        see: .env.example
+    "#);
+
+    let mysql_connection_pool = Pool::builder(
+        AsyncDieselConnectionManager::<diesel_async::AsyncMysqlConnection>
+        ::new(mysql_url)
+    )
+    .build()
+    .expect("failed to establish connection pooling");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(mysql_connection_pool.clone()))
             .configure(frontend::endpoints)
     })
     .bind(("127.0.0.1", 8080))?
