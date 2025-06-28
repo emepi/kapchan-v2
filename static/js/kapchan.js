7b0eb203200067d1c88abc06ecd52dbcdabe78be
emepi <elmeri.kemppainen@gmail.com>
2025-06-28 14:11:00 +0300

    Selector restyled

--- static/js/kapchan.js
+++ static/js/kapchan.js
@@ -90,3 +90,36 @@ const closeMobileMenu = () => {
   mobileCanvas.style.display = "none";
   mobileMenu.style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
+  let mouseDown = false;
+  let startX, scrollLeft;
+  const slider = document.querySelector('.selector');
+
+  const startDragging = (e) => {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  }
+  
+  const stopDragging = (e) => {
+    mouseDown = false;
+  }
+  
+  const move = (e) => {
+    e.preventDefault();
+    if(!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+  }
+
+  if (!supportsTouch) {
+    slider.addEventListener('mousemove', move, false);
+    slider.addEventListener('mousedown', startDragging, false);
+    slider.addEventListener('mouseup', stopDragging, false);
+    slider.addEventListener('mouseleave', stopDragging, false);
+  }
+});
